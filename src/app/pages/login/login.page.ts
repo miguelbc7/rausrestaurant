92fe81265010bda4c159eba0235143217df7f306
jesusrosales19 <jesusrosales19@gmail.com>
2019-11-06 12:14:50 -0400

    actualizado

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -63,51 +63,6 @@ export class LoginPage implements OnInit {
 
   }
 
-  // validation_messages = {
-  //   'username': [
-  //     { type: 'required', message: 'Username is required.' },
-  //     { type: 'minlength', message: 'Username must be at least 5 characters long.' },
-  //     { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
-  //     { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
-  //     { type: 'validUsername', message: 'Your username has already been taken.' }
-  //   ],
-  //   'name': [
-  //     { type: 'required', message: 'Name is required.' }
-  //   ],
-  //   'lastname': [
-  //     { type: 'required', message: 'Last name is required.' }
-  //   ],
-  //   'email': [
-  //     { type: 'required', message: 'Email is required.' },
-  //     { type: 'pattern', message: 'Please wnter a valid email.' }
-  //   ],
-  //   'phone': [
-  //     { type: 'required', message: 'Phone is required.' },
-  //     { type: 'validCountryPhone', message: 'The phone is incorrect for the selected country.' }
-  //   ],
-  //   'password': [
-  //     { type: 'required', message: 'Password is required.' },
-  //     { type: 'minlength', message: 'Password must be at least 5 characters long.' },
-  //     { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number.' }
-  //   ],
-  //   'confirm_password': [
-  //     { type: 'required', message: 'Confirm password is required.' }
-  //   ],
-  //   'matching_passwords': [
-  //     { type: 'areEqual', message: 'Password mismatch.' }
-  //   ],
-  //   'terms': [
-  //     { type: 'pattern', message: 'You must accept terms and conditions.' }
-  //   ],
-  // };
-
-  // onSubmit(values){
-  //   console.log(values);
-  //   this.router.navigate(["/home"]);
-  // }
-
-
-
   async presentPoliticas() {
     const modal = await this.modalCtrl.create({
       component: ModalPoliticasPage,
