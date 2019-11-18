4b9b5c147c2dba9a56ed72c2525f8475b7a5c881
jesusrosales19 <jesusrosales19@gmail.com>
2019-11-18 19:05:48 -0400

    modal excelente

---
+++ src/app/pages/modals/excelente/excelente.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excelente',
+  templateUrl: './excelente.page.html',
+  styleUrls: ['./excelente.page.scss'],
+})
+export class ExcelentePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
