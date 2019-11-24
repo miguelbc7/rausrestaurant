5eedf7cf1d2c641936e7c4f0c2513d81ffcc0392
jesusrosales19 <jesusrosales19@gmail.com>
2019-11-24 10:34:12 -0400

    images

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   //local
-  url: 'http://192.168.0.130:3800/api/',
+  // url: 'http://192.168.0.130:3800/api/',
   // url: 'http://localhost:3800/api/',
   //server
-  // url: 'http://35.197.211.133:8080/api/',
+   url: 'http://35.197.211.133:8080/api/',
   firebase: 
   {
     apiKey: "AIzaSyDMMmvx93kGW0ZiHpkbqFTopre63FHogzE",
