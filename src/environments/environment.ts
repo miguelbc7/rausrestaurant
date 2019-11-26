45679894059345e2a31c06569b0c1845537ff3e1
Humberto Andueza <humbertoanduezaa@gmail.com>
2019-11-26 08:40:19 -0430

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   // url: 'http://192.168.0.130:3800/api/',
   // url: 'http://localhost:3800/api/',
   //server
-   url: 'https://cors-anywhere.herokuapp.com/http://35.214.92.171:8080/api/',
+   url: 'http://35.214.92.171:8080/api/',
 
   firebase: 
   {
